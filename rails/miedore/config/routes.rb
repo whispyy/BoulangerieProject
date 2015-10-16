39b1bb8764ab67cf64b132bda8c7e734a1bb160c
JF-Dell <jfdurk@gmx.fr>
2015-10-16 21:15:33 +0200

    mvc

--- rails/miedore/config/routes.rb
+++ rails/miedore/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-
+  root :to => 'application#home'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
