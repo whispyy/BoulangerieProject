5ba6601b92164e882574b52f9abc007e6c5154f7
Tanguy <muselet.tanguy@gmail.com>
2015-10-17 00:17:49 +0200

    structure de l app

---
+++ controllers/appCtrl.js
@@ -0,0 +1,25 @@
+'use strict';
+
+mieDore.controller('appCtrl',['$scope', '$state', function($scope,$state){
+
+
+
+	$scope.message = "appCtrl";
+
+	
+
+	$scope.go = function(path) {
+		console.log(path);
+		$state.go(path);
+	};	
+ 	
+
+		
+	
+
+
+
+
+
+}]);
+
---
+++ controllers/homeCtrl.js
@@ -0,0 +1,20 @@
+'use strict';
+
+mieDore.controller('homeCtrl',['$scope', '$state', function($scope,$state){
+
+
+
+	$scope.message = "homeCtrl";
+
+
+ 	
+
+		
+	
+
+
+
+
+
+}]);
+
