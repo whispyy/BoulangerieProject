8a59870b11a4b47964d1c8bb8e7a2d694e8df61d
whispyy <jfdurk@hotmail.fr>
2015-10-20 11:08:19 +0200

    structure 'map' pour les produits

--- ProduitsCtrl2.js
+++ ProduitsCtrl2.js
@@ -32,12 +32,37 @@ mieDore.controller('produitsCtrl',['$scope', '$state', function($scope,$state){
 			nom : 'baguette',
 			prix : ,
 			desc : '',
-		}/*..etc etc*/
-		'baguette pavot',
-		'pain au blé',
-		'pains aux céréales',
-		'Marguerite sésame pavot',
-		'mont royal'];
+		},
+		{
+			id : 004,
+			nom : 'baguette pavot',
+			prix : ,
+			desc : '',
+		},
+		{
+			id : 005,
+			nom : 'pain au blé',
+			prix : ,
+			desc : '',
+		},
+		{
+			id : 006,
+			nom : 'pains aux céréales',
+			prix : ,
+			desc : '',
+		},
+		{
+			id : 007,
+			nom : 'Marguerite sésame pavot',
+			prix : ,
+			desc : '',
+		},
+		{
+			id : 008,
+			nom : 'mont royal',
+			prix : ,
+			desc : '',
+		}];
 	$scope.boolPatisserie = false;
 	$scope.fpatisserie = function() {
 		$scope.boolBoulangerie = false;
@@ -47,19 +72,68 @@ mieDore.controller('produitsCtrl',['$scope', '$state', function($scope,$state){
 
 	};
 	$scope.patisserie =
-		['gateau',
-		'individuelle',
-		'tarte'];
-	$scope.gateau = [
-		'Foret noire',
-		'Fraisier',
-		'Grand mille feuilles',
-		'Grand paris brest',
-		'Mousse abricot',
-		'Mousse fruits rouges',
-		'Senseo'];
+		[{
+			id : 101,
+			nom : 'gateau',
+		},
+		{
+			id : 102,
+			nom : 'individuelle',
+		},
+		{
+			id : 103,
+			nom : 'tarte',
+		}];
+	$scope.gateau = 
+		[{
+			id : 111,
+			nom : 'Foret noire',
+			prix : ,
+			desc : '',
+		},
+		{
+			id : 112,
+			nom : 'Fraisier',
+			prix : ,
+			desc : '',
+		},
+		{
+			id : 113,
+			nom : 'Grand mille feuilles',
+			prix : ,
+			desc : '',
+		},
+		{
+			id : 114,
+			nom : 'Grand paris brest',
+			prix : ,
+			desc : '',
+		},
+		{
+			id : 115,
+			nom : 'Mousse abricot',
+			prix : ,
+			desc : '',
+		},
+		{
+			id : 116,
+			nom :  'Mousse fruits rouges',
+			prix : ,
+			desc : '',
+		},
+		{
+			id : 117,
+			nom :  'Senseo',
+			prix : ,
+			desc : '',
+		}];
 	$scope.individuelle =
-		['Eclair au café',
+		[{
+			id : 121,
+			nom : 'Eclair au café',
+			prix : ,
+			desc : '',
+		 },/*etc*/
 		'Eclair au chocolat',
 		'Flan',
 		'Meringue chocolat',
@@ -68,7 +142,12 @@ mieDore.controller('produitsCtrl',['$scope', '$state', function($scope,$state){
 		'Religieuse au chocolat'
 		];
 	$scope.tarte =
-		['Tarte aux fraises',
+		[{
+			id : 131,
+			nom : 'Tarte aux fraises',
+			prix : ,
+			desc : '',
+		}, /*etc*/
 		'Tarte aux pommes',
 		'Tarte carachoc',
 		'Tarte au citron-meringue'];
@@ -81,7 +160,12 @@ mieDore.controller('produitsCtrl',['$scope', '$state', function($scope,$state){
 		$scope.boolViennoiserie = false;
 	};
 	$scope.sandwich =
-		['jambon emmental',
+		[{
+			id : 201 ,
+			nom : 'jambon emmental',
+			prix : ,
+			desc : '',
+		}/*etc*/
 		'jambon serrano',
 		'poulet et fromage de chèvre',
 		'poulet mayonnaise',
@@ -96,7 +180,12 @@ mieDore.controller('produitsCtrl',['$scope', '$state', function($scope,$state){
 		$scope.boolViennoiserie = !$scope.boolViennoiserie;
 	};
 	$scope.viennoiserie =
-		['abricotier',
+		[{
+			id : 301,
+			nom : 'abricotier',
+			prix : ,
+			desc : '',
+		}, /*etc*/
 		'amandier',
 		'brioche au chocolat',
 		'brioche nature',
