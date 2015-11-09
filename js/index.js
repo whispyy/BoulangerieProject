09871e1a192828eff7b8a945b5e849f00c86cc69
RousseAddict <rousseaddict@gmail.com>
2015-11-09 18:20:05 +0100

    inclus archive linux node

--- js/index.js
+++ js/index.js
@@ -1,8 +1,8 @@
 var server = require('./server');
-//var mongoose = require('mongoose');
+var mongoose = require('mongoose');
 
 
-//mongoose.connect('mongodb://localhost/BoulangerieProject', function(err) {
-//  if (err) { throw err; }
-//});
+mongoose.connect('mongodb://localhost/BoulangerieProject', function(err) {
+  if (err) { throw err; }
+});
 
