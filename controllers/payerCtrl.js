4f0ef97c57f3a42301f9b8ed264cbe03cdc74455
whispyy <jfdurk@hotmail.fr>
2015-11-16 10:05:38 +0100

    panier bdd en cours

--- controllers/payerCtrl.js
+++ controllers/payerCtrl.js
@@ -1,10 +1,24 @@
 'use strict';
 
-mieDore.controller('payerCtrl',['$scope', '$state', function($scope,$state){
+mieDore.controller('payerCtrl',['$scope', '$state', '$http', function($scope,$state,$http){
 
 
 	console.log("payerCtrl");
 	$scope.page = "payer";
+	$scope.add = function () {
+		$http({
+			method: 'POST',
+			url: 'controllers/panier.php'
+			data:{
+
+			},
+			headers: {'Content-Type' : 'application/x-www-form-urlencoded'}
+		})
+		.success(function(response){
+			console.log("pid :"+response);
+		})
+		
+	};
 
 }]);
 
